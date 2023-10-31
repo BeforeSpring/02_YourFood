1ba89f9ffda9eca3121a484777fa3d65ec7bf69b
Danbi <ddanhopark@gmail.com>
2023-10-31 21:52:35 +0900

    Init: Add repository
    member, menu, restaurant, review repository 추가
    Related to: #4, #5, #6, #7

---
+++ src/main/java/beforespring/yourfood/restaurant/domain/MemberRepository.java
@@ -0,0 +1,8 @@
+package beforespring.yourfood.restaurant.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
