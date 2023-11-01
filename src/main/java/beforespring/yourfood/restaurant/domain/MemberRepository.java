d60729313b3668b926135f7ed13203eae6570100
Danbi <ddanhopark@gmail.com>
2023-11-01 15:35:59 +0900

    Remove: git 충돌로 인해 삭제
    git 사용상 실수로 생긴 파일 삭제

--- src/main/java/beforespring/yourfood/restaurant/domain/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package beforespring.yourfood.restaurant.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
--- src/main/java/beforespring/yourfood/restaurant/domain/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package beforespring.yourfood.restaurant.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
