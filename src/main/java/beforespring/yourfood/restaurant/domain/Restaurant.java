fbc3aa59f784ad8b3f5847d3763c6d6e2a35aa91
Danbi <ddanhopark@gmail.com>
2023-11-01 16:14:11 +0900

    Refactor: Add field - 운영 여부를 확인하는 operating 필드 추가 - 삭제된 정보인지 확인하는 deleted 필드 추가

--- src/main/java/beforespring/yourfood/restaurant/domain/Restaurant.java
+++ src/main/java/beforespring/yourfood/restaurant/domain/Restaurant.java
@@ -57,19 +57,28 @@ public class Restaurant {
     @Column(nullable = false)
     private Double rating;
 
+    @Column(nullable = false, columnDefinition = "TINYINT(1)")
+    private boolean operating;
+
+    @Column(nullable = false, columnDefinition = "TINYINT(1)")
+    private boolean deleted;
+
     @Builder
     public Restaurant(
         String name,
         String description,
         String address,
         Coordinate coordinate,
-        CuisineType cuisineType) {
+        CuisineType cuisineType,
+        Boolean operating) {
         this.name = name;
         this.description = description;
         this.address = address;
         this.coordinate = coordinate;
         this.cuisineType = cuisineType;
         this.rating = 0.0;
+        this.operating = operating;
+        this.deleted = false;
     }
 
     /**
@@ -78,15 +87,25 @@ public class Restaurant {
      * @param description 수정할 식당 설명
      * @param address     수정할 식당 주소
      * @param coordinate  수정할 좌표
+     * @param operating   운영 여부
      */
     public void updateRestaurant(
         String description,
         String address,
         AddressCode addressCode,
-        Coordinate coordinate) {
+        Coordinate coordinate,
+        Boolean operating) {
         this.description = description;
         this.address = address;
         this.addressCode = addressCode;
         this.coordinate = coordinate;
+        this.operating = operating;
+    }
+
+    /**
+     * 레스토랑 db를 삭제 상태로 변경
+     */
+    public void deleteRestaurantDb() {
+        this.deleted = true;
     }
 }
