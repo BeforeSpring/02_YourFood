de70fea6ff6a8934a6d47e48d01796afc47b1264
Danbi <ddanhopark@gmail.com>
2023-11-02 10:24:18 +0900

    Refactor: review 등록, 수정 로직 수정
    review 등록에서 member는 memberId를 참조만 하는 객체로 변경
    review 수정에서 param의 mmeberId와 작성된 리뷰의 memberId가 일치 여부를 확인하는 로직 추가

--- src/main/java/beforespring/yourfood/app/review/service/ReviewService.java
+++ src/main/java/beforespring/yourfood/app/review/service/ReviewService.java
@@ -13,8 +13,9 @@ public interface ReviewService {
     /**
      * 리뷰 수정
      * @param reviewId 수정할 리뷰 id
+     * @param memberId 수정할 회원 id
      * @param content 리뷰 내용
      * @param rating 식당 평점
      */
-    public void updateReview(Long reviewId, String content, Integer rating);
+    public void updateReview(Long reviewId, Long memberId, String content, Integer rating);
 }
--- src/main/java/beforespring/yourfood/app/review/service/ReviewServiceImpl.java
+++ src/main/java/beforespring/yourfood/app/review/service/ReviewServiceImpl.java
@@ -1,14 +1,14 @@
 package beforespring.yourfood.app.review.service;
 
+import beforespring.yourfood.app.exception.RestaurantNotFoundException;
+import beforespring.yourfood.app.exception.ReviewNotFoundException;
 import beforespring.yourfood.app.member.domain.Member;
 import beforespring.yourfood.app.member.domain.MemberRepository;
 import beforespring.yourfood.app.restaurant.domain.Restaurant;
 import beforespring.yourfood.app.restaurant.domain.RestaurantRepository;
 import beforespring.yourfood.app.review.domain.Review;
 import beforespring.yourfood.app.review.domain.ReviewRepository;
-import beforespring.yourfood.app.exception.MemberNotFoundException;
-import beforespring.yourfood.app.exception.RestaurantNotFoundException;
-import beforespring.yourfood.app.exception.ReviewNotFoundException;
+import beforespring.yourfood.app.review.exception.MemberMismatchException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,7 +23,7 @@ public class ReviewServiceImpl implements ReviewService {
 
     @Override
     public void saveReview(Long memberId, Long restaurantId, String content, Integer rating) {
-        Member member = memberRepository.findById(memberId).orElseThrow(MemberNotFoundException::new);
+        Member member = memberRepository.getReferenceById(memberId);
         Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(RestaurantNotFoundException::new);
         Review review = Review.builder()
                             .member(member)
@@ -35,8 +35,11 @@ public class ReviewServiceImpl implements ReviewService {
     }
 
     @Override
-    public void updateReview(Long reviewId, String content, Integer rating) {
+    public void updateReview(Long reviewId, Long memberId, String content, Integer rating) {
         Review review = reviewRepository.findById(reviewId).orElseThrow(ReviewNotFoundException::new);
+        if (!memberId.equals(review.getMember().getId())) {
+            throw new MemberMismatchException();
+        }
         review.updateReview(content, rating);
     }
 }
