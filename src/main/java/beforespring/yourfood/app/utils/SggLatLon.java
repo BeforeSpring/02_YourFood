aacf1f668bf003f3c04e18620742703520df1c76
Danbi <ddanhopark@gmail.com>
2023-11-06 14:06:12 +0900

    Init: CsvIOException 추가
    CsvIOException 추가
    Related to #27

--- src/main/java/beforespring/yourfood/app/utils/SggLatLon.java
+++ src/main/java/beforespring/yourfood/app/utils/SggLatLon.java
@@ -2,29 +2,14 @@ package beforespring.yourfood.app.utils;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-
-@Entity
-@Table(
-    name = "sgg_lat_lon",
-    indexes = {
-        @Index(
-            name = "idx__si_do__si_gun_gu",
-            columnList = "si_do, si_gun_gu"
-        )
-    }
-)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class SggLatLon {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "sgg_lat_lon_id")
-    private Long id;
-    @Column(name = "si_do")
+
     private String siDo;
-    @Column(name = "si_gun_gu")
     private String siGunGu;
     private String lon;
     private String lat;
