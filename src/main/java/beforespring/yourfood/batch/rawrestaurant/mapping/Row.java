ffcfb0b8ac0edbdc1e158193b98eaf4cd7603d3e
jaekkang <rkdwornjs123@gmail.com>
2023-11-06 10:05:47 +0900

    Comment: Add comment in Row

--- src/main/java/beforespring/yourfood/batch/rawrestaurant/mapping/Row.java
+++ src/main/java/beforespring/yourfood/batch/rawrestaurant/mapping/Row.java
@@ -7,6 +7,17 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Row {
+    /**
+     * API 호출로 받은 데이터 중 <row /> 부분의 필드를 맵핑하기위해
+     * 각각 대입할 수 있는 타입으로 필드들을 만들었습니다.
+     *
+     * 각각 변수명을 필드명에 맞추면 알아서 해준다고 되어 있었는데
+     * @JacksonXmlProperty 어노테이션을 통해서 직접 맵핑을 해주지 않으면
+     * 에러가 나는 문제가 있어, 각각 직접 맵핑해주었습니다.
+     *
+     *  @JacksonXmlProperty(localName = "SIGUN_NM")에서
+     *  localName이 입력받은 Xml 데이터중에서 찾을 필드명을 입력하면 됩니다.
+     */
     @JacksonXmlProperty(localName = "SIGUN_NM") String sigunNm;
 
     @JacksonXmlProperty(localName = "SIGUN_CD") String sigunCd;
