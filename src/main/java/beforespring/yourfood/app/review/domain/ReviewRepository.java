035cf5454e96ffdc9604a6d55c546dbe12318be8
Danbi <ddanhopark@gmail.com>
2023-11-08 23:59:06 +0900

    Feat: 평점 미반영 리뷰 조회 쿼리 추가
    - 수정된 리뷰 중 레스토랑 평점에 미반영된 리뷰 조회 쿼리 추가
    - 새로운 리뷰 중 레스토랑 평점에 미반영된 리뷰 조회 쿼리 추가
    Related to #28

--- src/main/java/beforespring/yourfood/app/review/domain/ReviewRepository.java
+++ src/main/java/beforespring/yourfood/app/review/domain/ReviewRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
@@ -13,4 +15,10 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @Query("SELECT r FROM Review r WHERE r.restaurant.id = :restaurantId")
     Page<Review> findReviewsByRestaurantIdPaged(@Param("restaurantId") Long restaurantId, Pageable pageable);
+
+    @Query("SELECT r FROM Review r WHERE r.updatedAt < :restaurantRatingUpdatedAt AND r.reflectedRating = false")
+    List<Review> findUnreflectedRatingModifiedReviews(@Param("restaurantRatingUpdatedAt") LocalDateTime restaurantRatingUpdatedAt);
+
+    @Query("SELECT r FROM Review r WHERE r.updatedAt IS NULL AND r.reflectedRating = false")
+    List<Review> findUnreflectedRatingNewReviews();
 }
