7e9246a1de53cba092aa4244b8e0153533f5646c
Danbi <ddanhopark@gmail.com>
2023-11-08 18:21:42 +0900

    Feat: 레스토랑 평점 업데이트 메서드 추가
    - 새로운 리뷰 평점 업데이트
    - 수정된 리뷰 평점 업데이트
    Related to #28

--- src/main/java/beforespring/yourfood/app/restaurant/service/RestaurantService.java
+++ src/main/java/beforespring/yourfood/app/restaurant/service/RestaurantService.java
@@ -4,4 +4,20 @@ import beforespring.yourfood.app.restaurant.service.dto.RestaurantWithReviewDto;
 
 public interface RestaurantService {
     RestaurantWithReviewDto getRestaurantDetail(Long restaurantId);
+
+    /**
+     * 새로운 리뷰의 평점을 식당 평점에 업데이트함
+     *
+     * @param restaurantId 업데이트 할 레스토랑 id
+     * @param reviewId     수정된 리뷰 id
+     */
+    public boolean updateNewReviewRating(Long restaurantId, Long reviewId);
+
+    /**
+     * 수정된 리뷰의 평점을 식당 평점에 업데이트함
+     *
+     * @param restaurantId 업데이트 할 레스토랑 id
+     * @param reviewId     수정된 리뷰 id
+     */
+    public boolean updateReviewRating(Long restaurantId, Long reviewId);
 }
--- src/main/java/beforespring/yourfood/app/restaurant/service/RestaurantServiceImpl.java
+++ src/main/java/beforespring/yourfood/app/restaurant/service/RestaurantServiceImpl.java
@@ -4,19 +4,25 @@ import beforespring.yourfood.app.restaurant.domain.Restaurant;
 import beforespring.yourfood.app.restaurant.domain.RestaurantRepository;
 import beforespring.yourfood.app.restaurant.exception.RestaurantNotFoundException;
 import beforespring.yourfood.app.restaurant.service.dto.RestaurantWithReviewDto;
-import beforespring.yourfood.app.review.domain.Review;
 import beforespring.yourfood.app.restaurant.service.dto.ReviewDto;
+import beforespring.yourfood.app.review.domain.Review;
 import beforespring.yourfood.app.review.domain.ReviewRepository;
+import beforespring.yourfood.app.review.exception.ReviewNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class RestaurantServiceImpl implements RestaurantService {
     private final RestaurantRepository restaurantRepository;
     private final ReviewRepository reviewRepository;
 
+    @Override
+    @Transactional(readOnly = true)
     public RestaurantWithReviewDto getRestaurantDetail(Long restaurantId) {
         Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(RestaurantNotFoundException::new);
         List<Review> reviews = reviewRepository.findByRestaurantId(restaurantId);
@@ -26,5 +32,18 @@ public class RestaurantServiceImpl implements RestaurantService {
 
     }
 
+    @Override
+    public boolean updateNewReviewRating(Long restaurantId, Long reviewId) {
+        Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(RestaurantNotFoundException::new);
+        Review review = reviewRepository.findById(reviewId).orElseThrow(ReviewNotFoundException::new);
+        return restaurant.updateNewReviewRating(review);
+    }
+
+    @Override
+    public boolean updateReviewRating(Long restaurantId, Long reviewId) {
+        Restaurant restaurant = restaurantRepository.findById(restaurantId).orElseThrow(RestaurantNotFoundException::new);
+        Review review = reviewRepository.findById(reviewId).orElseThrow(ReviewNotFoundException::new);
+        return restaurant.updateModifiedReviewRating(review);
+    }
 }
 
