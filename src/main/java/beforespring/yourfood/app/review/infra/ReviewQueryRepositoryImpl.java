923baa12baad70151fcff2264e49231cc1686b52
yhk313 <kyeong4099@naver.com>
2023-11-26 03:23:24 +0900

    Refactor: 동적 정렬, 카운트 쿼리 추가하여 수정
    - countReviews 메서드 추가하여 리뷰 수를 카운트 쿼리로 조회

--- src/main/java/beforespring/yourfood/app/review/infra/ReviewQueryRepositoryImpl.java
+++ src/main/java/beforespring/yourfood/app/review/infra/ReviewQueryRepositoryImpl.java
@@ -2,21 +2,16 @@ package beforespring.yourfood.app.review.infra;
 
 import beforespring.yourfood.app.review.domain.Review;
 import beforespring.yourfood.app.review.domain.ReviewQueryRepository;
-import beforespring.yourfood.app.review.exception.InvalidOrderByException;
 import beforespring.yourfood.app.utils.OrderBy;
 import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.core.types.dsl.DateTimePath;
-import com.querydsl.core.types.dsl.NumberPath;
-import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.core.types.dsl.ComparableExpressionBase;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static beforespring.yourfood.app.review.domain.QReview.review;
@@ -32,32 +27,30 @@ public class ReviewQueryRepositoryImpl implements ReviewQueryRepository {
 
     @Override
     public Page<Review> findReviewsByRestaurantIdOrderBy(boolean desc, OrderBy orderBy, Long restaurantId, Pageable pageable) {
-        JPAQuery<Review> query = jpaQueryFactory.selectFrom(review)
-            .where(review.restaurantId.eq(restaurantId));
-        if (orderBy != null) {
-            query.orderBy(getOrderSpecifier(orderBy, desc));
-        }
-
-        List<Review> reviews = query
+        List<Review> reviews = jpaQueryFactory.selectFrom(review)
+            .where(review.restaurantId.eq(restaurantId))
+            .orderBy(getOrderSpecifier(orderBy, desc))
             .offset(pageable.getOffset())
             .limit(pageable.getPageSize())
             .fetch();
-
-        return new PageImpl<>(reviews, pageable, reviews.size());
+        return PageableExecutionUtils.getPage(reviews, pageable, () -> countReviews(restaurantId));
     }
 
     private OrderSpecifier<?> getOrderSpecifier(OrderBy orderBy, boolean desc) {
-        NumberPath<Integer> rating = review.rating;
-        DateTimePath<LocalDateTime> createdAt = review.createdAt;
+        ComparableExpressionBase<?> condition = switch (orderBy) {
+            case RATING -> review.rating;
+            case TIME -> review.createdAt;
+            default -> review.rating;
+        };
+        return desc ? condition.desc() : condition.asc();
+    }
 
-        switch (orderBy) {
-            case RATING -> {
-                return desc ? rating.desc() : rating.asc();
-            }
-            case TIME -> {
-                return desc ? createdAt.desc() : createdAt.asc();
-            }
-            default -> throw new InvalidOrderByException(orderBy.name());
-        }
+    public long countReviews(Long restaurantId) {
+        //noinspection ConstantConditions
+        return jpaQueryFactory
+            .select(review.count())
+            .from(review)
+            .where(review.restaurantId.eq(restaurantId))
+            .fetchOne();
     }
 }
