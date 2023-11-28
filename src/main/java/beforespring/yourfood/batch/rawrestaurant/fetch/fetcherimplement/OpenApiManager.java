446ec80bf737378a0d3dcf1be4dca932b6f551e6
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2023-11-28 16:08:12 +0900

    fix: SneakyThrows 잘못 사용하여 발생한 문제

--- src/main/java/beforespring/yourfood/batch/rawrestaurant/fetch/fetcherimplement/OpenApiManager.java
+++ src/main/java/beforespring/yourfood/batch/rawrestaurant/fetch/fetcherimplement/OpenApiManager.java
@@ -1,6 +1,5 @@
 package beforespring.yourfood.batch.rawrestaurant.fetch.fetcherimplement;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.SneakyThrows;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -9,7 +8,7 @@ public class OpenApiManager {
     private final PagingQueryHttpUriBase pagingQueryHttpUriBase;
     private final RestTemplate restTemplate;
 
-    @SneakyThrows(JsonProcessingException.class)
+    @SneakyThrows
     public ResponseEntity<String> fetch(int page, int pageSize) {
         // 간혹 XML 응답 양식 공공API ContentType 헤더가 JSON으로 설정된 경우가 있어서 문자열로 받은 후 XML 매퍼를 사용하도록 지정함.
         return restTemplate.getForEntity(
