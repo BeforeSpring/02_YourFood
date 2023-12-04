3b87437cd71b2ab5943fcedb25c536a89e69c194
yhk313 <kyeong4099@naver.com>
2023-12-04 23:57:08 +0900

    Refactor: record 로 변경
    - CuisineInfo -> CuisineGroup 이름 변경
    - CuisineGroup 매개변수 타입 변경

--- src/main/java/beforespring/yourfood/app/restaurant/service/dto/RestaurantInfo.java
+++ src/main/java/beforespring/yourfood/app/restaurant/service/dto/RestaurantInfo.java
@@ -1,20 +1,19 @@
 package beforespring.yourfood.app.restaurant.service.dto;
+import beforespring.yourfood.app.restaurant.domain.Restaurant;
 import lombok.*;
 
 import java.math.BigDecimal;
 
-@Getter
-public class RestaurantInfo {
-    private String name;
-    private BigDecimal rating;
-    private String description;
-    private String address;
-
+public record RestaurantInfo(String name, BigDecimal rating, String description, String address) {
     @Builder
-    public RestaurantInfo(String name, BigDecimal rating, String description, String address) {
-        this.name = name;
-        this.rating = rating;
-        this.description = description;
-        this.address = address;
+    public RestaurantInfo {
+    }
+    public static RestaurantInfo createFromRestaurant(Restaurant restaurant) {
+        return new RestaurantInfo(
+            restaurant.getName(),
+            restaurant.getRating(),
+            restaurant.getDescription(),
+            restaurant.getAddress()
+        );
     }
 }
