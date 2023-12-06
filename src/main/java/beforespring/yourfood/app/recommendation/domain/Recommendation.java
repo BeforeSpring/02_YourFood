8f99639c5ef38d354439d393fe75e5f38fe4dbb6
yhk313 <kyeong4099@naver.com>
2023-12-06 10:54:05 +0900

    Chore: 주석 추가

--- src/main/java/beforespring/yourfood/app/recommendation/domain/Recommendation.java
+++ src/main/java/beforespring/yourfood/app/recommendation/domain/Recommendation.java
@@ -5,9 +5,6 @@ import beforespring.yourfood.app.restaurant.service.dto.CuisineGroup;
 
 import java.util.List;
 
-/**
- * 추천 데이터. 구현 필요.
- */
 public record Recommendation(
         Subscriber subscriber,
         List<CuisineGroup> recommendation
--- src/main/java/beforespring/yourfood/app/recommendation/infra/RecommenderImpl.java
+++ src/main/java/beforespring/yourfood/app/recommendation/infra/RecommenderImpl.java
@@ -6,17 +6,30 @@ import beforespring.yourfood.app.recommendation.domain.Subscriber;
 import beforespring.yourfood.app.restaurant.service.RestaurantService;
 import beforespring.yourfood.app.restaurant.service.dto.CuisineGroup;
 import beforespring.yourfood.app.utils.OrderBy;
+
 import java.util.List;
 
 /**
- * 빈 주입을 위해 임시로 만들어둔 클래스.
+ * 위치기반으로 주변 식당을 카테고리별로 추천한다.
+ * <ol>
+ *  <li>반경 n미터 내에 있는 음식점을 조회한다.</li>
+ *  <li>카테고리 별로 음식점을 그룹화 한다.</li>
+ *  <li>평점순으로 상위 n개의 음식점믈 반환한다.</li>
+ * </ol>
  */
 public class RecommenderImpl implements Recommender {
+
     private final RestaurantService restaurantService;
     private final int rangeInMeters;
     private final int limitByCuisineType;
 
-    public RecommenderImpl(RestaurantService restaurantService, int rangeInMeters, int limitByCuisineType) {
+    /**
+     * @param restaurantService  음식점 서비스
+     * @param rangeInMeters      범위 (미터)
+     * @param limitByCuisineType 카테고리별 음식점 제한 수
+     */
+    public RecommenderImpl(RestaurantService restaurantService, int rangeInMeters,
+        int limitByCuisineType) {
         this.restaurantService = restaurantService;
         this.rangeInMeters = rangeInMeters;
         this.limitByCuisineType = limitByCuisineType;
@@ -24,7 +37,12 @@ public class RecommenderImpl implements Recommender {
 
     @Override
     public Recommendation getRecommendation(Subscriber subscriber) {
-        List<CuisineGroup> cuisineGroups = restaurantService.getRestaurantGroupedBy(OrderBy.RATING, true, subscriber.coordinates(), rangeInMeters, limitByCuisineType);
+        List<CuisineGroup> cuisineGroups = restaurantService.getRestaurantGroupedBy(
+            OrderBy.RATING,
+            true, // true: 오름차순 정렬, false: 내림차순 정렬
+            subscriber.coordinates(),
+            rangeInMeters,
+            limitByCuisineType);
         return new Recommendation(subscriber, cuisineGroups);
     }
 }
